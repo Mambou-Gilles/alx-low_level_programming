c78f76b479281d10b5f72a632009db18cef0aada
eliegilles <gillesravelmambou@gmail.com>
2023-06-22 19:56:40 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,24 +16,28 @@ int main(void)
 		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
-			printf(" ");
 		}
-		else if (i % 3 == 0)
+		else if (i % 3 == 0 && !(i % 5 == 0))
 		{
 			printf("Fizz");
-			printf(" ");
 		}
-		else if (i % 5 == 0)
+		else if (i % 5 == 0 && !(i % 3 == 0))
 		{
 			printf("Buzz");
-			printf(" ");
 		}
 		else
 		{
 			printf("%d", i);
+		}
+		if (i != 100)
+		{
 			printf(" ");
 		}
+		else
+		{
+			printf("\n");
+
+		}
 	}
-	printf("\n");
 	return (0);
 }
